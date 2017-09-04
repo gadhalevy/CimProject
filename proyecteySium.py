8d5dbf7d6591bbb9022f3b023d937c1b1ceca4b9
gadh <gadh@gmail.com>
2017-09-04 14:14:33 +0300

    Added delete project and go back editing.

--- proyecteySium.py
+++ proyecteySium.py
@@ -9,6 +9,7 @@ from wtforms.validators import DataRequired
 from wtforms import StringField,SubmitField,IntegerField,ValidationError,SelectField,PasswordField
 from wtforms.widgets import TextArea
 import os
+import sqlalchemy
 
 # import flask_psycopg2
 app = Flask(__name__)
@@ -17,7 +18,7 @@ Bootstrap(app)
 
 app.secret_key = 'development key'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-engine=create_engine(app)
+engine=sqlalchemy(app)
 # engine = create_engine('postgresql:///try.db')
 # engine = create_engine('sqlite:///try.db')
 Base.metadata.create_all(engine)
